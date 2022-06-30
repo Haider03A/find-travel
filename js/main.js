dbc93e94a98124a702fb2c325cded7d6b10189fd
Hayder Pc <you@example.com>
2022-06-30 12:10:48 +0300

    from pc

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+const showAndHiddenEle = (eleForHidden, eleStatusCB) => {
+    const eleStatus = eleForHidden.dataset.show === 'true';
+    eleForHidden.dataset.show = !eleStatus;
+    eleStatusCB(eleStatus)
+};
+
 const menuIcon = document.querySelector("header > nav > .menu-icon");
 const menu = document.querySelector("header > nav > .menu");
+const menulinks = document.querySelectorAll(".menu .nav ul li");
+const menuConfig = document.querySelectorAll(".menu .config ul li");
+const menuSocial = document.querySelectorAll(".menu .social ul li");
 
+menuIcon.addEventListener('click', function () {
+    showAndHiddenEle(menu, status => {
+        this.dataset.open = !status;
+        const anim = eles => {
+            eles.forEach((link, i) => {
+                if (!status) {
+                    link.style.animation = `translate-link .4s forwards ${i * 0.1}s`;
+                } else {
+                    link.style.animation = null;
+                }
+            });
+        };
+        anim(menulinks);
+        anim(menuConfig);
+        anim(menuSocial);
+
+    });
+
+});
